5585299ac00ee6c54e72af0d55790fb7235e4910
Borja22 <bmontoroplaza.cf@iesesteveterradas.cat>
2022-09-16 19:24:15 +0200

    Inicio

--- app/src/main/java/com/example/appnummesgranmespetit/MainActivity.java
+++ app/src/main/java/com/example/appnummesgranmespetit/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.appnummesgranmespetit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int numSec=22;
+        final EditText et=findViewById(R.id.NumeroIntroducido);
+        final Button b=findViewById(R.id.enviar);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                if (Integer.parseInt(et.getText().toString())==numSec){
+                    CharSequence text = "Has adivinado el numero!";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else if (Integer.parseInt(et.getText().toString())<numSec){
+                    CharSequence text = "El numero introducido es mas pequeño que el que tienes que adivinar!";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else{
+                    CharSequence text = "El numero introducido es mas grande que el que tienes que adivinar!";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
     }
 }
